a9ec602c2579cf13fb37298bcfb80a8d67fe6b36
Chanaka Dulanjan <108602041+98Chanaka@users.noreply.github.com>
2024-06-14 10:33:04 +0530

    first commit

---
+++ Online-Fast-food-delievery-Project/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; } 
+        public int ItemId { get; set; }
+        public item Item { get; set; } 
+        public String ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        [Required,MinLength(1)]
+        public int Count { get; set; }
+
+
+    }
+}
